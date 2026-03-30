499db058be0d6bee8ce861cb7fb4a104a618a05c
Marvelloust <omoleyemarvellous@gmail.com>
2026-03-30 19:01:07 +0100

    Update.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Nexentrix/',
+  base: '/Nexentrix-company-profile/',
   plugins: [react()],
 })
